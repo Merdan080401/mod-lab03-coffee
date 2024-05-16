3b8ef9ebbb11d0559684cc291bef3d54339dad7b
Merdan <163035166+Merdan080401@users.noreply.github.com>
2024-05-16 23:09:39 +0300

    Add files via upload

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,47 @@
-// Copyright 2022 GHA Test Team
-
-#include <gtest/gtest.h>
 #include "Automata.h"
+#include <cassert>
+
+void testOnOff() {
+    Automata machine;
+    assert(machine.getState() == Automata::OFF);
+    machine.on();
+    assert(machine.getState() == Automata::WAIT);
+    machine.off();
+    assert(machine.getState() == Automata::OFF);
+}
+
+void testCoin() {
+    Automata machine;
+    machine.on();
+    machine.coin(1.0);
+    assert(machine.getState() == Automata::ACCEPT);
+}
+
+void testChoice() {
+    Automata machine;
+    const std::string menu[] = {"Coffee", "Tea", "Milk"};
+    const double prices[] = {1.5, 1.0, 0.75};
+    machine.getMenu(menu, prices, 3);
+    machine.on();
+    machine.coin(2.0);
+    machine.choice(1);
+    assert(machine.check());
+}
+
+void testCancel() {
+    Automata machine;
+    machine.on();
+    machine.coin(1.0);
+    machine.cancel();
+    assert(machine.getState() == Automata::WAIT);
+    assert(machine.cash == 0);
+}
+
+int main() {
+    testOnOff();
+    testCoin();
+    testChoice();
+    testCancel();
+
+    return 0;
+}
